67966c99c26711c075017b5b9b217f431f5d45ed
Arthique S <137800145+ARTHIQUE@users.noreply.github.com>
2023-10-30 11:53:59 +0530

    Time: 1488 ms (99.16%), Space: 0B (100%) - LeetHub

---
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+with cte as(
+    select *,dense_rank()over(partition by product_id order by year)as rnk
+    from sales
+
+)select product_id,year as first_year, quantity, price
+from cte
+where rnk=1
